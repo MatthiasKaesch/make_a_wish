2d23d72bb3d0a089d27151dca813480c9151a1b5
Matthias Cash <77434788+MatthiasKaesch@users.noreply.github.com>
2021-05-25 18:29:22 +0200

    adding HashRouter for SPA on gh-pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Suspense, useCallback, useState, useEffect } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
 import classes from './App.module.css';
 
 import Auth from './components/Auth/AuthForm';
 import Register from './components/Register/RegisterForm';
-import Profile from './components/Profile/Profile';
-import FriendList from './components/FriendList/FriendList';
+import Profile from './components/Profile/Profile'; /* 
+import FriendList from './components/FriendList/FriendList'; */
 
 const MainNavigation = React.lazy(() => import('./components/Layout/MainNavigation'));
 const WishList = React.lazy(() => import('./components/WishList/WishList'));
@@ -17,7 +17,7 @@ function App() {
   const [wishes, setWishes] = useState([]);
   const [uid, setUid] = useState('');
 
-  const [friends, setFriends] = useState([{ name: '' }]);
+  /*   const [friends, setFriends] = useState([{ name: '' }]); */
 
   const onPassUidHandler = (fetchedUid) => {
     setUid(fetchedUid);
@@ -90,37 +90,39 @@ function App() {
   return (
     <main>
       <div className={classes.app}>
-        <Route path="">
-          <Redirect to="/login" />
-        </Route>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Route path="/login">
-            <Auth onPassUid={onPassUidHandler} />
+        <HashRouter>
+          <Route path="">
+            <Redirect to="/login" />
           </Route>
-          <Route path="/profile">
-            <MainNavigation />
-            <Profile uid={uid} />
-          </Route>
-          {/*  <Route path="/friendList">
+          <Suspense fallback={<p>Loading...</p>}>
+            <Route path="/login">
+              <Auth onPassUid={onPassUidHandler} />
+            </Route>
+            <Route path="/profile">
+              <MainNavigation />
+              <Profile uid={uid} />
+            </Route>
+            {/*  <Route path="/friendList">
             <MainNavigation />
             <FriendList friends={friends} />
           </Route> */}
-          <Route path="/register">
-            <Register onPassUid={onPassUidHandler} />
-          </Route>
-          {!isLoading && !error && (
-            <Route path="/my_wishlist">
-              <MainNavigation />
-              <WishList onRemoveWish={wishDeleteHandler} wishes={wishes} />
+            <Route path="/register">
+              <Register onPassUid={onPassUidHandler} />
             </Route>
-          )}
-          {!isLoading && !error && (
-            <Route path="/new_wish">
-              <MainNavigation />
-              <NewWish onAddWish={wishAddHandler} />
-            </Route>
-          )}
-        </Suspense>
+            {!isLoading && !error && (
+              <Route path="/my_wishlist">
+                <MainNavigation />
+                <WishList onRemoveWish={wishDeleteHandler} wishes={wishes} />
+              </Route>
+            )}
+            {!isLoading && !error && (
+              <Route path="/new_wish">
+                <MainNavigation />
+                <NewWish onAddWish={wishAddHandler} />
+              </Route>
+            )}
+          </Suspense>
+        </HashRouter>
       </div>
     </main>
   );
