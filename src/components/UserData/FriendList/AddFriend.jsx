6ee17a5a2fc801ff25647741ab5bb79a230cc730
Matthias Cash <77434788+MatthiasKaesch@users.noreply.github.com>
2021-06-02 18:17:06 +0200

    adding search for users functionality

--- src/components/UserData/FriendList/AddFriend.jsx
+++ src/components/UserData/FriendList/AddFriend.jsx
@@ -1,17 +1,45 @@
 import classes from './AddFriend.module.css';
 
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
 
 import BackButton from '../BackButton/BackButton';
 
-const FriendList = () => {
+const AddFriend = () => {
   const [searchMode, setSearchMode] = useState(false);
+  const [fetchedUser, setFetchedUser] = useState([]);
+  const [loading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const searchInput = useRef('');
 
   const toggleHandler = () => {
     setSearchMode((searchMode) => !searchMode);
   };
 
-  const addFriendHandler = () => {};
+  const fetchUserHandler = useCallback(async () => {
+    const searchString = searchInput.current.value;
+
+    if (searchString !== '') {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          `https://make-a-wish-2c068-default-rtdb.europe-west1.firebasedatabase.app/users/${searchString}.json`
+        );
+        if (!response.ok) {
+          throw new Error('Something went wrong!');
+        }
+        const data = await response.json();
+        /*  console.log(data); */
+
+        const user = [data.name];
+        setFetchedUser(user);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    }
+  }, []);
 
   return (
     <div>
@@ -23,23 +51,23 @@ const FriendList = () => {
         {searchMode ? (
           <div className={classes.addFriendInput}>
             <p>User Name:</p>
-            <input type="text" />
+            <input ref={searchInput} type="text" />
           </div>
         ) : (
           <div className={classes.addFriendInput}>
             <p>User ID:</p>
-            <input type="text" />
+            <input ref={searchInput} type="text" />
           </div>
         )}
-        <button className={classes.searchButton} onClick={addFriendHandler}>
+        <button className={classes.searchButton} onClick={fetchUserHandler}>
           SEARCH
         </button>
         <BackButton path="/friendList" />
       </section>
-
+      <p>{fetchedUser}</p>
       <hr />
     </div>
   );
 };
 
-export default FriendList;
+export default AddFriend;
