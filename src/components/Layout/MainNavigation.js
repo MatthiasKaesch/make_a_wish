dfd591ed0c2865acf753932ec93539b65e15ef20
Matthias Cash <77434788+MatthiasKaesch@users.noreply.github.com>
2021-06-02 16:26:05 +0200

    Optimizing CSS file and class setup

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,7 +1,10 @@
+import classes from './MainNavigation.module.css';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
-import './MainNavigation.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
@@ -14,15 +17,15 @@ const MainNavigation = () => {
 
   return (
     <header>
-      <nav className={'navigation'}>
+      <nav className={classes.navigation}>
         <ul>
           <li>{isLoggedIn && <Link to="my_wishlist">Wishlist</Link>}</li>
-          {/*           <li>{isLoggedIn && <Link to="friendList"> Friends</Link>}</li> */}
+          {<li>{isLoggedIn && <Link to="friendList"> Friends</Link>}</li>}
           <li>{isLoggedIn && <Link to="profile"> Profile</Link>}</li>
-          <li>
+          <li className={classes.logoutButton}>
             {isLoggedIn && (
               <Link to="/login" onClick={logoutHandler}>
-                Logout
+                <FontAwesomeIcon icon={faSignOutAlt} />
               </Link>
             )}
           </li>
