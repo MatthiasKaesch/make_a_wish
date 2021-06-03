730d87a74c88fe91307272401db96425853da908
Matthias Cash <77434788+MatthiasKaesch@users.noreply.github.com>
2021-06-03 17:33:09 +0200

    adding user search and technical friend request

--- src/components/UserData/FriendList/AddFriend.jsx
+++ src/components/UserData/FriendList/AddFriend.jsx
@@ -1,79 +1,122 @@
 import classes from './AddFriend.module.css';
 
-import React, { useState, useCallback, useRef } from 'react';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import BackButton from '../BackButton/BackButton';
+import Spinner from '../../Spinner/Spinner';
 
-const AddFriend = () => {
-  const [searchMode, setSearchMode] = useState(false);
+const AddFriend = (props) => {
   const [fetchedUser, setFetchedUser] = useState([]);
+  const [fetchedUserId, setFetchedUserId] = useState('');
+  const [fetchedProfile, setFetchedProfile] = useState({ name: '', uid: '' });
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(true);
 
+  const url = 'https://make-a-wish-2c068-default-rtdb.europe-west1.firebasedatabase.app';
   const searchInput = useRef('');
-
-  const toggleHandler = () => {
-    setSearchMode(!searchMode);
-    console.log(searchMode);
-  };
 
   const fetchUserHandler = useCallback(async () => {
     const searchString = searchInput.current.value;
 
     if (searchString === '') {
-      setError('Please enter a name or ID');
+      setError('Please enter an ID');
       return;
     }
     setError(null);
+    setIsLoading(true);
     try {
-      const response = await fetch(
-        `https://make-a-wish-2c068-default-rtdb.europe-west1.firebasedatabase.app/users/${searchString}.json`
-      );
-      if (!response.ok) {
+      const foundUsersResponse = await fetch(`${url}/users/${searchString}.json`);
+
+      if (!foundUsersResponse.ok) {
         throw new Error('Something went wrong!');
       }
-      const data = await response.json();
-      const user = [data.name];
-      setFetchedUser(user);
+      const data = await foundUsersResponse.json();
+
+      setFetchedUser(data.name);
+      setFetchedUserId(data.uid);
     } catch (error) {
       setFetchedUser('');
       switch (error.message) {
         case 'data is null':
-          setError('No user with that name was found');
+          setError('No user with that ID found');
           break;
         default:
           setError('');
       }
     }
+    setIsLoading(false);
   }, []);
+
+  async function friendRequestHandler() {
+    console.log('Friend Added');
+
+    fetchProfileHandler();
+
+    const friendEntry = {};
+    friendEntry[props.uid] = fetchedProfile[0].name;
+    console.log(friendEntry);
+
+    await fetch(`${url}/users/${fetchedUserId}/friends.json`, {
+      method: 'PUT',
+      body: JSON.stringify(friendEntry),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  const fetchProfileHandler = useCallback(async () => {
+    if (props.uid !== '') {
+      try {
+        const response = await fetch(
+          `https://make-a-wish-2c068-default-rtdb.europe-west1.firebasedatabase.app/users/${props.uid}.json`
+        );
+        if (!response.ok) {
+          throw new Error('Something went wrong!');
+        }
+        const data = await response.json();
+
+        const profileData = [];
+
+        profileData.push({
+          name: data.name,
+          uid: data.uid,
+        });
+        setFetchedProfile(profileData);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+  }, [props.uid]);
 
   return (
     <div>
       <section>
         <h2>Add new friend</h2>
-        <button className={classes.toggleButton} onClick={toggleHandler}>
-          {!searchMode ? 'Search by User ID' : 'Search by name'}
-        </button>
 
         <div className={classes.searchUser}>
-          <p>{searchMode ? 'User ID:' : 'User Name:'}</p>
+          <p>User ID:</p>
           <input ref={searchInput} type="text" />
         </div>
 
         <button className={classes.searchButton} onClick={fetchUserHandler}>
           SEARCH
         </button>
-        {error ? (
-          <p className={classes.error}>{error}</p>
-        ) : (
-          <div>
-            <p className={classes.user}>{fetchedUser}</p>
-            <button>
-              <FontAwesomeIcon className={classes.addButton} icon={faPlus} />
-            </button>
-          </div>
-        )}
+        {isLoading && <Spinner />}
+        {!isLoading &&
+          (error ? (
+            <p className={classes.error}>{error}</p>
+          ) : (
+            <div className={classes.userContainer}>
+              <h3>{fetchedUser}</h3>
+              <button onClick={friendRequestHandler}>
+                <FontAwesomeIcon className={classes.addButton} icon={faPlus} />
+              </button>
+            </div>
+          ))}
+
         <hr />
         <BackButton path="/friendList" />
       </section>
